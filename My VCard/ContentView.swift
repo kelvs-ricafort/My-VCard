7b5c6c4258f1487897b97776766f0c8886a8376c
Kelvin Ricafort <kelvs.r@outlook.com>
2023-09-25 23:03:11 +0800

    Initial Commit

---
+++ My VCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  My VCard
+//
+//  Created by Kelvin Ricafort on 9/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ My VCard/My_VCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_VCardApp.swift
+//  My VCard
+//
+//  Created by Kelvin Ricafort on 9/25/23.
+//
+
+import SwiftUI
+
+@main
+struct My_VCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
